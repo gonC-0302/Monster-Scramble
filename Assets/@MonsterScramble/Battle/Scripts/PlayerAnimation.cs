d8d88311bf65db8ce78e21ffc6970f98d63509c2
gonC-0302 <gonc0302@gmail.com>
2024-05-06 17:38:15 +0900

    [add] プレイヤーのアニメーション制御スクリプトを追加

---
+++ Assets/@MonsterScramble/Battle/Scripts/PlayerAnimation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animation))]
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _anim;
+    static readonly int BLEND_KEY = Animator.StringToHash("Blend");
+
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    public void PlayMoveAnimation(float speed)
+    {
+        _anim.SetFloat(BLEND_KEY, speed);
+    }
+}
